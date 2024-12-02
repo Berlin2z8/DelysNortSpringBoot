e2d065cdbb404e93ca9c395e98e18e57c74e2117
Marx Alonso <marxchip99@gmail.com>
2024-12-01 22:34:14 -0500

    intento de sistema de pago

---
+++ src/main/java/com/marxchipana/DelysNortSpringBoot/models/PayPalClient.java
@@ -0,0 +1,11 @@
+package com.marxchipana.DelysNortSpringBoot.models;
+
+
+public class PayPalClient {
+
+    private static final String CLIENT_ID = "AewCjIDEEGGrDDlPnrDxw0HHhVphtTbgi3N81YJnHzRSKzkad8Kv1oAcanIMBobqmgraC_nbuNKxsBr6";
+    private static final String CLIENT_SECRET = "EHn1UpXSsFdHJBmh_G7KUzfoZfM7qfqf8KGRjR_RXvFR4z_bHPChJWHxF0V3C5EzP6mCjux9AN320Qoi";
+    private static final String MODE = "sandbox"; // Cambiar a "live" en producción
+
+}
+
